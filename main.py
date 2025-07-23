b435c4d7b71933af3576fd1bdaecd3995fa8441b
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-07-23 21:18:16 +0100

    feat: returning summary data correctly

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import jsonify
+from routes.forecast import return_forecast
 
 app = Flask(__name__)
 
 @app.route('/forecast')
 def get_forecast():
-    return jsonify({'message': 'Forecast endpoint'})
+    response = return_forecast()
+    print(response)
+    return jsonify({'message': 'response'})
 
 
 app.run(debug=True, host='0.0.0.0', port=5000)
