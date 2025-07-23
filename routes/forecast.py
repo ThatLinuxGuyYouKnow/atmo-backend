fc21e7b6af62c44fdb1c770f9681d6ad5a4947b6
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-07-23 20:49:58 +0100

    fix: looping through minutely prediction

--- routes/forecast.py
+++ routes/forecast.py
@@ -1,7 +1,24 @@
-import requests
 import os
+import requests
+from methods.getWeatherNarration import generateWeatherSummary
 
 apikey = os.environ['TOMORROW_KEY']
-async def returnForecast(longlat: str) :
-    response = await requests.get(f'https://api.tomorrow.io/v4/weather/forecast?location=${longlat}&apikey=${apikey}')
-    values = response['timelines']['minutely']
+
+def return_forecast(longlat: str):
+    """Fetches minute-by-minute forecast and returns a list of narration strings."""
+ 
+    url = f"https://api.tomorrow.io/v4/weather/forecast?location={longlat}&apikey={apikey}"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+
+ 
+    values = data.get("timelines", {}).get("minutely", [])
+    narration_list = []
+
+  
+    for minute_data in values:
+        summary = generateWeatherSummary(minute_data)
+        narration_list.append(summary)
+
+    return narration_list
