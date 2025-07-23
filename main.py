8f32a13e7e0aeffda59a04f4f52a4262df79372d
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-07-23 23:39:21 +0100

    chore: restricted /forecast method

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from flask import Flask
+
+from flask import Flask,request
 from flask import jsonify
 from routes.forecast import return_forecast
 
 app = Flask(__name__)
 
-@app.route('/forecast')
+@app.route('/forecast',methods=['GET', 'POST'])
 def get_forecast(location_name):
-    response = return_forecast(longlat=location_name)
-    print(response)
-    return jsonify({'message': 'response'})
+    if request.method == 'POST':
+
+     response = return_forecast(longlat=location_name)
+     print(response)
+     return jsonify({'message': 'response'})
 
 
 app.run(debug=True, host='0.0.0.0', port=5000)
